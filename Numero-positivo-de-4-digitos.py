058d93f3c0d760e9c71b3f857676f8b40fe362bf
Juan22345020 <126175834+Juan22345020@users.noreply.github.com>
2023-03-06 17:03:02 -0500

    Add files via upload

---
+++ Numero-positivo-de-4-digitos.py
@@ -0,0 +1,27 @@
+
+print("------------------------------------------------")
+print("---------------Positivo y de 4 digitos----------")
+print("------------------------------------------------")
+
+# input
+
+x = int(input("Digite el numero: "))
+
+
+
+# procesing
+
+if x>0 :
+    if x>999 and x<10000 :
+        print("el numero es positivo y de 4 digitos")
+    else:
+        print("positivo pero no de 4 digitos")
+
+else:
+    print("No es positivo")    
+    
+    
+    
+print("------------------------------------------------")
+print("---------------Resultados----------------------")
+print("------------------------------------------------")
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Numero-positivo-de-4-digitos
+
+# Analisis
+
+# Diseño
+
+## Diagrama de flujo
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
